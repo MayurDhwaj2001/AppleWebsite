04eb210b6eccd1ac44ac06b2a77740d8b727022e
Mayur Dhwaj <mayurdhwaj1315@gmail.com>
2023-05-04 14:09:31 +0530

    hi

--- script.js
+++ script.js
@@ -26,7 +26,6 @@ const captchanumber=document.getElementById("captcha");
 const refreshbutton=document.getElementById("refresh");
 let random;
 let randomString;
-
 function ran(){
     random=Math.floor(Math.random() * 10000);
     randomString = String(random).padStart(4, "0");
@@ -64,3 +63,46 @@ fromcheck.addEventListener("input",checkcaptcha);
 
 // password validation
 
+const pass=document.getElementById("pass");
+const confirmpass=document.getElementById("confirmpass");
+
+function checkpass(){
+
+    const passval=pass.value;
+    const passlen=pass.value.length;
+
+    const capital = /[ABCDEFGHIJKLMNOPQRSTUVWXYZ]/;
+    const small = /[abcdefghijklmnopqrstuvwxyz]/;
+    const numbers = /[0123456789]/;
+    const symbols = /[!@#$%^&*()<>?:|~`]/;
+    const containscap = capital.test(passval);
+    const containssmall = small.test(passval);
+    const containsnum = numbers.test(passval);
+    const containssymbol = symbols.test(passval);
+    let passsatisfied=0;
+    
+    if(passlen<8){                                         
+        console.log("Should be more than 7");
+    }
+    if(!containscap){                                      
+        console.log("Contains Capital Case");
+    }
+    if(!containssmall){
+        console.log("Contains Small Case");
+    }
+    if(!containsnum){
+        console.log("Contains Numbers");
+    }
+    if(!containssymbol){
+        console.log("Contains Symbols");
+    }
+    if(passlen>7 && containscap && containssmall && containsnum && containssymbol){
+        console.log("satisfy");             
+        pass.style.border="2px solid green" ;                  
+    }
+    else{
+        pass.style.border="1px solid grey" ;                  
+    }
+}
+
+pass.addEventListener("input",checkpass);
